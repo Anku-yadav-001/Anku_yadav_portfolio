139965b82d29eab4003181d4ab74f3e1ede67a9e
Anku-yadav-001 <yaman49751@gmail.com>
2024-07-01 21:20:01 +0530

    done

--- script.js
+++ script.js
@@ -11,12 +11,46 @@ let name=document.getElementById('name');
 let email=document.getElementById('email');
 let number=document.getElementById("phone");
 let message=document.getElementById('messagesend');
+let file=document.getElementById('file');
 
-function sendEmail(){
+// function sendEmail(){
+//     let messagebody=`Full Name:     ${name.value} <br>
+//                      Mobile Number: ${number.value}<br>
+//                      Email Address: ${email.value}<br>
+//                      Resume:        ${file.value}<br>
+//                      Message :      ${message.value} `
+            
+    // Email.send({
+    //     Host : "smtp.elasticemail.com",
+    //     Username : "amany49751@gmail.com",
+    //     Password : "FF172C23AF276A4BB0869C65D9E41A217CAD",
+    //     To : 'amany49751@gmail.com',
+    //     From : "amany49751@gmail.com",
+    //     Subject : "Portfolio Contact Request",
+    //     Body : messagebody
+    // }).then(
+    //   message => {
+    //         if(message=="OK"){
+    //             Swal.fire({
+    //                 title: "Success!",
+    //                 text: "Message sent successfully!",
+    //                 icon: "success"
+    //               });
+    //         }
+           
+    //   }
+    // );
+// }
+
+function sendEmail() {
     let messagebody=`Full Name:     ${name.value} <br>
-                     Mobile Number: ${number.value}<br>
-                     Email Address: ${email.value}<br>
-                     Message :      ${message.value} `
+                        Mobile Number: ${number.value}<br>
+                        Email Address: ${email.value}<br>
+                        Resume:        ${file.value}<br>
+                        Message :      ${message.value} `
+    const reader = new FileReader();
+    reader.readAsDataURL(file.FILES[0]);
+
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "amany49751@gmail.com",
@@ -24,25 +58,21 @@ function sendEmail(){
         To : 'amany49751@gmail.com',
         From : "amany49751@gmail.com",
         Subject : "Portfolio Contact Request",
-        Body : messagebody
-    }).then(
-      message => {
-            if(message=="OK"){
-                Swal.fire({
-                    title: "Success!",
-                    text: "Message sent successfully!",
-                    icon: "success"
-                  });
-            }
-           
-      }
-    );
+        Body : messagebody,
+        Attachments: [
+            {
+                name: "File_Name_with_Extension",
+                path: reader.result``
+            }]
+    })
+        .then(function (message) {
+            alert("Mail has been sent successfully")
+        });
 }
 
 from.addEventListener('submit',(e)=>{
     e.preventDefault();
-    sendEmail();
-    
+    sendEmail();    
 })
 
 // let id=document.getElementById('meriid')
